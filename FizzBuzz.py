c4fa923fd029b0a2b1ebb23b94415847794b0f9c
hanxheng <69658775+hanxheng@users.noreply.github.com>
2020-09-02 08:19:53 +0800

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,13 @@
+##2/9/2020
+##FizzBuzz
+
+for x in range(1, 21):
+    if x % 3 == 0 and x % 5 == 0:
+        print('FizzBuzz')
+    elif x % 3 == 0:
+        print('Fizz')
+    elif x % 5 == 0:
+        print('Buzz')
+    else:
+        print(x)
+        
